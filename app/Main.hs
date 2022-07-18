66a5aad4a53c0c535bf41ca2e10c729121d3ae04
Gabriel Kabbe <gabriel.kabbe@mail.de>
2022-07-18 11:06:55 +0200

    Encoding and decoding works

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,25 @@
 module Main where
 
 import Control.Monad (guard)
+import Control.Monad.State.Lazy
 import Data.Char (ord)
 
 import qualified Enigma as E
 import qualified Wheel as W
 
+runEnigma :: [Char] -> Maybe ([Char], E.Enigma)
+runEnigma input = runStateT (forM input E.step) enigma
+ where
+  enigma = E.initEnigma W.w1 (W.Offset 0) W.w2 (W.Offset 0) W.w3 (W.Offset 0) W.ukwa (E.Steckerbrett [])
+
 main :: IO ()
-main = print (W.makeWheel (['B' .. 'Z'] ++ "A") (W.charToNotch 'X') (W.charToTurnover 'X'))
+main = do
+  case runEnigma "HELLOWORLD" of
+    Just (encoded, _) -> do
+      putStrLn encoded
+      case runEnigma encoded of
+        Just (decoded, _) -> do
+          putStrLn decoded
+        _ -> putStrLn "oops"
+    _ -> putStrLn "oops"
+  return ()
--- src/Enigma.hs
+++ src/Enigma.hs
@@ -1,25 +1,30 @@
 module Enigma where
 
+import Control.Monad.State (State)
+import Control.Monad.State.Lazy (StateT, get, lift, put)
 import qualified Wheel as W
 
 newtype Steckerbrett = Steckerbrett [(Char, Char)] deriving (Show)
 
 data Enigma = Enigma W.WheelState W.WheelState W.WheelState W.Umkehrwalze Steckerbrett deriving (Show)
 
-step :: Enigma -> Char -> Maybe (Enigma, Char)
-step enigma@(Enigma wst1 wst2 wst3 ukw stkbr) c0 = do
-    c1 <- W.forward wst1 c0
-    c2 <- W.forward wst2 c1
-    c3 <- W.forward wst3 c2
-    c4 <- W.forward ukw c3
-    c5 <- W.backward wst3 c4
-    c6 <- W.backward wst2 c5
-    c7 <- W.backward wst1 c6
+step :: Char -> StateT Enigma Maybe Char
+step c0 = do
+    (Enigma wst1 wst2 wst3 ukw stkbr) <- get
+    c1 <- lift $ W.forward wst1 c0
+    c2 <- lift $ W.forward wst2 c1
+    c3 <- lift $ W.forward wst3 c2
+    c4 <- lift $ W.forward ukw c3
+    c5 <- lift $ W.backward wst3 c4
+    c6 <- lift $ W.backward wst2 c5
+    c7 <- lift $ W.backward wst1 c6
 
     let wst3New = W.rotate wst3
     let wst2New = if W.isAtTurnover wst3 then W.rotate wst2 else wst2
     let wst1New = if W.isAtTurnover wst2 then W.rotate wst1 else wst1
-    return (Enigma wst1New wst2New wst3New ukw stkbr, c7)
+
+    put (Enigma wst1New wst2New wst3New ukw stkbr)
+    return c7
 
 initEnigma :: W.Wheel -> W.Offset -> W.Wheel -> W.Offset -> W.Wheel -> W.Offset -> W.Umkehrwalze -> Steckerbrett -> Enigma
 initEnigma whl1 offset1 whl2 offset2 whl3 offset3 = Enigma (W.WheelState whl1 offset1) (W.WheelState whl2 offset2) (W.WheelState whl3 offset3)
