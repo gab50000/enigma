7bd74f8a4c2113e734ca56a3a3b48401826a081d
Gabriel Kabbe <gabriel.kabbe@mail.de>
2022-07-19 19:55:07 +0200

    Add second pass through of Steckerbrett

--- src/Enigma.hs
+++ src/Enigma.hs
@@ -4,6 +4,7 @@ import Control.Monad.State (State)
 import Control.Monad.State.Lazy (StateT, forM, get, lift, put, runStateT)
 
 import SteckerBrett as SB
+import Wheel (Umkehrwalze (Umkehrwalze))
 import qualified Wheel as SB
 import qualified Wheel as W
 
@@ -12,10 +13,16 @@ data Enigma = Enigma W.WheelState W.WheelState W.WheelState W.Umkehrwalze Stecke
 step :: Char -> StateT Enigma Maybe Char
 step c0 = do
     (Enigma wst1 wst2 wst3 ukw stkbr) <- get
+    -- pass through Steckerbrett
     c1 <- lift $ SB.forward stkbr c0
+    -- pass through wheels
     c2 <- lift $ W.forward wst1 c1 >>= W.forward wst2 >>= W.forward wst3
+    -- pass through Umkehrwalze
     c4 <- lift $ W.forward ukw c2
+    -- pass through wheels in reverse direction
     c5 <- lift $ W.forward (W.revert wst3) c4 >>= W.forward (W.revert wst2) >>= W.forward (W.revert wst1)
+    -- pass through Steckerbrett again
+    c6 <- lift $ SB.forward stkbr c5
 
     let wst3New = W.rotate wst3
     let wst2New = if W.isAtTurnover wst3 then W.rotate wst2 else wst2
