c29b75f194dc62a33177ccbe71f0180668e3e37d
Gabriel Kabbe <gabriel.kabbe@mail.de>
2022-07-19 19:50:07 +0200

    Update Main

--- app/Main.hs
+++ app/Main.hs
@@ -5,11 +5,12 @@ import Control.Monad.State.Lazy
 import Data.Char (ord)
 
 import qualified Enigma as E
+import qualified SteckerBrett as SB
 import qualified Wheel as W
 
 main :: IO ()
 main = do
-  let enigma = E.initEnigma W.w1 (W.Offset 0) W.w2 (W.Offset 0) W.w3 (W.Offset 0) W.ukwa (E.Steckerbrett [])
+  let enigma = E.initEnigma W.w1 (W.Offset 0) W.w2 (W.Offset 0) W.w3 (W.Offset 0) W.ukwa (SB.fromList [])
   case E.runEnigma enigma "HELLOWORLD" of
     Just encoded -> do
       putStrLn encoded
