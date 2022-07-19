59341dba37df3131ea7f497df19e14ce9abced11
Gabriel Kabbe <gabriel.kabbe@mail.de>
2022-07-19 19:50:03 +0200

    Update Enigma

--- src/Enigma.hs
+++ src/Enigma.hs
@@ -2,29 +2,27 @@ module Enigma where
 
 import Control.Monad.State (State)
 import Control.Monad.State.Lazy (StateT, forM, get, lift, put, runStateT)
-import qualified Wheel as W
 
-newtype Steckerbrett = Steckerbrett [(Char, Char)] deriving (Show)
+import SteckerBrett as SB
+import qualified Wheel as SB
+import qualified Wheel as W
 
 data Enigma = Enigma W.WheelState W.WheelState W.WheelState W.Umkehrwalze Steckerbrett deriving (Show)
 
 step :: Char -> StateT Enigma Maybe Char
 step c0 = do
     (Enigma wst1 wst2 wst3 ukw stkbr) <- get
-    c1 <- lift $ W.forward wst1 c0
-    c2 <- lift $ W.forward wst2 c1
-    c3 <- lift $ W.forward wst3 c2
-    c4 <- lift $ W.forward ukw c3
-    c5 <- lift $ W.backward wst3 c4
-    c6 <- lift $ W.backward wst2 c5
-    c7 <- lift $ W.backward wst1 c6
+    c1 <- lift $ SB.forward stkbr c0
+    c2 <- lift $ W.forward wst1 c1 >>= W.forward wst2 >>= W.forward wst3
+    c4 <- lift $ W.forward ukw c2
+    c5 <- lift $ W.forward (W.revert wst3) c4 >>= W.forward (W.revert wst2) >>= W.forward (W.revert wst1)
 
     let wst3New = W.rotate wst3
     let wst2New = if W.isAtTurnover wst3 then W.rotate wst2 else wst2
     let wst1New = if W.isAtTurnover wst2 then W.rotate wst1 else wst1
 
     put (Enigma wst1New wst2New wst3New ukw stkbr)
-    return c7
+    return c5
 
 initEnigma :: W.Wheel -> W.Offset -> W.Wheel -> W.Offset -> W.Wheel -> W.Offset -> W.Umkehrwalze -> Steckerbrett -> Enigma
 initEnigma whl1 offset1 whl2 offset2 whl3 offset3 = Enigma (W.WheelState whl1 offset1) (W.WheelState whl2 offset2) (W.WheelState whl3 offset3)
