10a8310712ba5a73a9e04b7026e9d6e56491b48c
Gabriel Kabbe <gabriel.kabbe@mail.de>
2022-07-20 22:08:24 +0200

    Fix enigma step

--- src/Enigma.hs
+++ src/Enigma.hs
@@ -12,24 +12,26 @@ data Enigma = Enigma W.WheelState W.WheelState W.WheelState W.Umkehrwalze Stecke
 
 step :: Char -> StateT Enigma Maybe Char
 step c0 = do
-    (Enigma wst1 wst2 wst3 ukw stkbr) <- get
+    enigma <- get
+    let newEnigma@(Enigma wst1 wst2 wst3 ukw stkbr) = updateState enigma
+    put newEnigma
     -- pass through Steckerbrett
     c1 <- lift $ SB.forward stkbr c0
     -- pass through wheels going from right to left
     c2 <- lift $ W.forward wst3 c1 >>= W.forward wst2 >>= W.forward wst1
     -- pass through Umkehrwalze
-    c4 <- lift $ W.forward ukw c2
+    c3 <- lift $ W.forward ukw c2
     -- pass through wheels in reverse direction
-    c5 <- lift $ W.forward (W.revert wst1) c4 >>= W.forward (W.revert wst2) >>= W.forward (W.revert wst3)
+    c4 <- lift $ W.forward (W.revert wst1) c3 >>= W.forward (W.revert wst2) >>= W.forward (W.revert wst3)
     -- pass through Steckerbrett again
-    c6 <- lift $ SB.forward stkbr c5
+    lift $ SB.forward stkbr c4
 
+updateState :: Enigma -> Enigma
+updateState (Enigma wst1 wst2 wst3 ukw stkbr) =
     let wst3New = W.rotate wst3
-    let wst2New = if W.isAtTurnover wst3 then W.rotate wst2 else wst2
-    let wst1New = if W.isAtTurnover wst2 then W.rotate wst1 else wst1
-
-    put (Enigma wst1New wst2New wst3New ukw stkbr)
-    return c5
+        wst2New = if W.isAtTurnover wst3 then W.rotate wst2 else wst2
+        wst1New = if W.isAtTurnover wst2 then W.rotate wst1 else wst1
+     in Enigma wst1New wst2New wst3New ukw stkbr
 
 initEnigma :: W.Wheel -> W.Offset -> W.Wheel -> W.Offset -> W.Wheel -> W.Offset -> W.Umkehrwalze -> Steckerbrett -> Enigma
 initEnigma whl1 offset1 whl2 offset2 whl3 offset3 = Enigma (W.WheelState whl1 offset1) (W.WheelState whl2 offset2) (W.WheelState whl3 offset3)
